d44b71d797fa5ead9bacd8dca6b08e7d765f4704
boki37 <bogdan.gojgic@gmail.com>
2026-02-08 23:55:50 +0100

    Add files via upload

---
+++ .next/dev/static/chunks/src_app_(auth)_login_page_tsx_d623e5a0._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_223386db._.js",
+  "static/chunks/src_d151d6de._.js"
+],
+    source: "dynamic"
+});
