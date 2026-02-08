1e1251ddc39249b99c02941c841dcbf4dd0d2407
boki37 <bogdan.gojgic@gmail.com>
2026-02-08 23:41:32 +0100

    Add files via upload

---
+++ prisma/migrations/20260204151145_optional_ticket_user/migration.sql
@@ -0,0 +1,17 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Ticket" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" INTEGER,
+    "eventId" INTEGER NOT NULL,
+    "purchasedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" TEXT NOT NULL DEFAULT 'VALID',
+    CONSTRAINT "Ticket_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Ticket_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Ticket" ("eventId", "id", "purchasedAt", "status", "userId") SELECT "eventId", "id", "purchasedAt", "status", "userId" FROM "Ticket";
+DROP TABLE "Ticket";
+ALTER TABLE "new_Ticket" RENAME TO "Ticket";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
